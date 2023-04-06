b4e94e3e6cdc239ced1e535fbcdd1e178e88b6d3
breeghty <kal7411@kakao.com>
2023-04-06 10:49:42 +0900

    printbakery

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('click', function(e) {
+    if (e.target.matches('a[href="#"]')) {
+        e.preventDefault();
+    }
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: false,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
